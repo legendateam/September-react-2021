1224a6234da928df60698c3e7475b593d43bd9e7
legendateam <undertaker176@gmail.com>
2022-01-30 02:01:07 +0200

    cw4

--- src/App.js
+++ src/App.js
@@ -1,55 +1,76 @@
-import React, {useReducer} from "react";
+import React, {useReducer} from 'react';
+
+import './App.css'
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case "INPUT":
-            return {...state, [action.payload.target.name] : action.payload.target.value}
-        case "SAVE_CAT":
-            return {...state, saveCat: action.payload.target.cat.value}
-        case "SAVE_DOG":
-            return {...state, saveDog: action.payload.target.dog.value}
-        case "DELETE_DOG" :
-            return {...state,  dog: "", saveDog: ""}
-        case "DELETE_CAT" :
-            return  {...state, cat: "", saveCat: ""}
-        case "DOGECOIN":
-            return {...state, arr: [...state.arr,{[action.payload.target.dog.value]: action.payload.target.dog.value}]}
-        default:
+        case 'ADD_DOG':
+            return {...state, dog: action.payload}
+        case 'ADD_CAT':
+            return {...state, cat: action.payload}
+        case 'SAVE_DOG':
+            return {...state, arrDog: action.payload}
+        case 'SAVE_CAT':
+            return {...state, arrCat: action.payload}
+        case 'DELETE_CAT':
+            return {...state, delete: state.arrCat.splice(action.payload, 1)}
+        case 'DELETE_DOG':
+            return {...state, delete: state.arrDog.splice(action.payload, 1)}
+        default :
             return state
     }
 }
 
 const App = () => {
-    const [state, dispatch] = useReducer(reducer, {dog: "", cat: "", saveDog: "",saveCat: "", arr: []});
+    const [state, dispatch] = useReducer(reducer, {dog: "", cat: "", arrDog: [], arrCat: [], delete: []});
 
-    const onSendDog = e => {
-        e.preventDefault();
-        dispatch({type: "SAVE_DOG", payload: e})
-        dispatch({type: "DOGECOIN", payload: e})
-        console.log(state.arr);
+
+    const deleteDog = i => {
+        dispatch({type: 'DELETE_DOG', payload: i})
     }
-    const onSendCat = e => {
-        e.preventDefault();
-        dispatch({type: "SAVE_CAT", payload: e})
+
+    const deleteCat = i => {
+        dispatch({type: 'DELETE_CAT', payload: i})
     }
+
+    const submitDog = e => {
+        e.preventDefault();
+        dispatch({type:'SAVE_DOG', payload: [...state.arrDog, {dog: e.target.dog.value}] })
+    }
+    const submitCat = e => {
+        e.preventDefault();
+        dispatch({type:'SAVE_CAT', payload: [...state.arrCat,{cat: e.target.cat.value}] })
+    }
+
     return (
         <div>
-            <form onSubmit={onSendDog}>
-                <label>add dog<input type="text" name="dog" value={state.dog} onChange={e => dispatch({type: "INPUT", payload: e})}/></label>
-                <button>save</button>
-            </form>
-            <form onSubmit={onSendCat}>
-                <label>add cat<input type="text" name="cat" value={state.cat} onChange={e => dispatch({type: "INPUT", payload: e})}/></label>
-                <button>save</button>
-            </form>
+            <div className={'form_animals'}>
+                <form onSubmit={submitDog}>
+                    <label>Add Dog: <input type="text" name={'dog'} value={state.dog} onChange={(e)=> dispatch({type: 'ADD_DOG', payload:e.target.value})}/></label>
+                    <button>save</button>
+                </form>
+                <form onSubmit={submitCat}>
+                    <label>Add Cat: <input type="text" name={'cat'} value={state.cat} onChange={(e)=> dispatch({type:'ADD_CAT', payload: e.target.value})}/></label>
+                    <button>save</button>
+                </form>
+            </div>
             <hr/>
-            <div>
-                {!!state.arr.length && state.arr.map((ar, i) => <div key={ar+i}>{ar}<button onClick={() => dispatch({type: "DELETE_DOG"})}>delete</button></div>)}
-                {state.saveCat && <div>{state.saveCat} <button onClick={() => dispatch({type: "DELETE_CAT", payload: ""})}>delete</button></div>}
+
+            <div className={'main_content'}>
+                <div>
+                    {state.arrDog.map((dog,i) => <div key={i}>
+                        {dog.dog} <button onClick={()=> deleteDog(i)}>DELETE</button>
+                    </div>)}
+                </div>
+
+                <div>
+                    {state.arrCat.map((cat,i) => <div key={i}>
+                        {cat.cat} <button onClick={()=> deleteCat(i)}>DELETE</button>
+                    </div>)}
+                </div>
             </div>
         </div>
     );
 };
 
 export default App;
-
